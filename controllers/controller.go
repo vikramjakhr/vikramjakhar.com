88b27d49d626b6b61a0b457b315a14ae76001e9f
vikram-jakhar <vikram.jakhr@gmail.com>
2016-09-04 12:18:58 +0530

    refactored

--- controllers/controller.go
+++ controllers/controller.go
@@ -5,35 +5,19 @@ import (
 	"html/template"
 	"strings"
 	"github.com/choudhary92/vikramjakhar.com/data"
-	"fmt"
 	"strconv"
-	"io/ioutil"
-	"encoding/json"
-	"os"
 )
 
 func init() {
 	registerHandlers()
+	parseMainLayout()
+	parseLayoutWithoutSidebar()
 }
 
 var pagination int64 = 5
 
 func Root(w http.ResponseWriter, r *http.Request) {
 	Pagination(w, r)
-}
-
-type BlogMetadata struct {
-	ID               int64  `json:"id"`
-	Name             string `json:"name"`
-	Title            string `json:"title"`
-	TmplFileName     string `json:"tmplFileName"`
-	Author           string `json:"author"`
-	PublishDate      int64 `json:"publishDate"`
-	Tags             string `json:"tags"`
-	CommentCount     int64 `json:"commentCount"`
-	ImageUrl         string `json:"imageUrl"`
-	CommentsFileName string `json:"commentsFileName"`
-	Summary          string `json:"summary"`
 }
 
 func Pagination(w http.ResponseWriter, r *http.Request) {
@@ -44,50 +28,25 @@ func Pagination(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			page = 1
 		}
-		var fileName string
-		var t, max int64 = 1, data.BlogMetaInfo.TotalBlogs
-		if page > 0 && (page * pagination) <= data.BlogMetaInfo.TotalBlogs {
-			t = (page * pagination) / 20
-			fileName = "blog-" + strconv.FormatInt(t * 20 + 1, 10) + "-to-" + strconv.FormatInt(t * 20 + 20, 10) + ".json"
-			max = pagination
-		} else {
-			page = 1
-			t = 1
-			max = data.BlogMetaInfo.TotalBlogs
-			fileName = "blog-1-to-20.json"
-		}
-
-		_, err = os.Stat("data/blogs/" + fileName)
+		fileName, max, t := GetFileName(page)
+		blogs, err := GetAllPostsOfPage(fileName)
 		if err != nil {
 			NotFound(w, r)
 			return
 		}
-		blogsData, err := ioutil.ReadFile("data/blogs/" + fileName)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Unable to find data"), http.StatusInternalServerError)
-			return
-		}
-		var blogsMetadata []BlogMetadata
-
-		if err := json.Unmarshal(blogsData, &blogsMetadata); err != nil {
-			http.Error(w, fmt.Sprintf("Unable to find blogs data"), http.StatusInternalServerError)
-			panic(err)
-			return
-		}
-
 		a := (page * pagination) - (t * 20)
 		if a < 1 {
 			a = 1
 		}
-		templateInfo := data.TemplateInfo{Title:"Vikram Jakhar", Name:"summary-tmpl", Path:"views/posts/summary-tmpl.html"}
-		renderMainLayout(w, r, templateInfo, templateInfo.Title, blogsMetadata[a - 1:a - 1 + max])
+		templateInfo := data.TemplateInfo{Title:"Vikram Jakhar", Name:"summary-tmpl", Path:postsPath + "summary-tmpl.html"}
+		renderMainLayout(w, r, templateInfo, templateInfo.Title, blogs[a - 1:a - 1 + max])
 	default:
 		http.Error(w, "Method not Allowd", http.StatusMethodNotAllowed)
 	}
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	templateInfo := data.TemplateInfo{Title:"About | Vikram Jakhar", Name:"about-tmpl", Path:"views/about/about-tmpl.html"}
+	templateInfo := data.TemplateInfo{Title:"About | Vikram Jakhar", Name:"about-tmpl", Path:aboutPath + "about-tmpl.html"}
 	renderLayoutWithoutSidebar(w, r, templateInfo, templateInfo.Title, nil)
 }
 
@@ -110,12 +69,12 @@ func Post(w http.ResponseWriter, r *http.Request) {
 		NotFound(w, r)
 		return
 	}
-	post, err := execTmplToString(blogMeta.Name, "views/posts/" + blogMeta.TmplFileName, blogMeta)
+	post, err := execTmplToString(blogMeta.Name, postsPath + blogMeta.TmplFileName, blogMeta)
 	if err != nil {
 		NotFound(w, r)
 		return
 	}
-	tmplInfo := data.TemplateInfo{Title:blogMeta.Title, Name:"post-tmpl", Path:"views/posts/post-tmpl.html"}
+	tmplInfo := data.TemplateInfo{Title:blogMeta.Title, Name:"post-tmpl", Path:postsPath + "post-tmpl.html"}
 	renderMainLayout(w, r, tmplInfo, blogMeta.Title, template.HTML(post))
 }
 
@@ -127,23 +86,4 @@ func NotFound(w http.ResponseWriter, r *http.Request) {
 func InternalError(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles("views/err/500.html")
 	tmpl.Execute(w, nil)
-}
-
-func GetPostInfo(pName, fileName string, pos int64) (BlogMetadata, error) {
-	blogMeta := BlogMetadata{}
-	_, err := os.Stat("data/blogs/" + fileName)
-	if err != nil {
-		return blogMeta, err
-	}
-	blogsData, err := ioutil.ReadFile("data/blogs/" + fileName)
-	if err != nil {
-		return blogMeta, err
-	}
-	var blogsMetadata []BlogMetadata
-	if err := json.Unmarshal(blogsData, &blogsMetadata); err != nil {
-		return blogMeta, err
-	}
-	//TODO : pointing to same slice, use copy
-	blogMeta = blogsMetadata[pos]
-	return blogMeta, nil
 }
--- controllers/helper.go
+++ controllers/helper.go
@@ -6,12 +6,21 @@ import (
 	"bytes"
 	"github.com/choudhary92/vikramjakhar.com/data"
 	"log"
+	"os"
+	"io/ioutil"
+	"encoding/json"
+	"strconv"
 )
 
 const (
 	mainLayout string = "main-layout"
 	layoutWithoutSidebar string = "layout-without-sidebar"
+	postsPath string = "views/posts/"
+	aboutPath string = "views/about/"
 )
+
+var parsedMainLayout *template.Template
+var parsedLayoutWithoutSidebar *template.Template
 
 type TmplContext struct {
 	Content template.HTML
@@ -28,18 +37,24 @@ func registerHandlers() {
 	http.Handle("/assets/", http.FileServer(http.Dir("views")))
 }
 
-func parseMainLayout() (*template.Template, error) {
-	tmpl, err := template.ParseFiles("views/layout/main-layout.html", "views/templates/login-signup-tmpl.html",
+func parseMainLayout() {
+	var err error
+	parsedMainLayout, err = template.ParseFiles("views/layout/main-layout.html", "views/templates/login-signup-tmpl.html",
 		"views/templates/footer.html", "views/templates/header.html", "views/templates/reside-bar-tmpl.html",
 		"views/templates/reside-bar-search-tmpl.html", "views/templates/reside-bar-social-tmpl.html",
 		"views/templates/reside-bar-latest-tmpl.html", "views/templates/reside-bar-widget-tmpl.html")
-	return tmpl, err
+	if err != nil {
+		log.Fatal("Error while parsing main-layout")
+	}
 }
 
-func parseLayoutWithoutSidebar() (*template.Template, error) {
-	tmpl, err := template.ParseFiles("views/layout/layout-without-sidebar.html", "views/templates/login-signup-tmpl.html",
+func parseLayoutWithoutSidebar() {
+	var err error
+	parsedLayoutWithoutSidebar, err = template.ParseFiles("views/layout/layout-without-sidebar.html", "views/templates/login-signup-tmpl.html",
 		"views/templates/footer.html", "views/templates/header.html")
-	return tmpl, err
+	if err != nil {
+		log.Fatal("Error while parsing main-layout")
+	}
 }
 
 func execTmplToString(tmplName, path string, data interface{}) (string, error) {
@@ -61,20 +76,66 @@ func executeTemplate(w http.ResponseWriter, tmpl *template.Template, name string
 
 func renderMainLayout(w http.ResponseWriter, r *http.Request, t data.TemplateInfo, title string, data interface{}) {
 	tmplString, _ := execTmplToString(t.Name, t.Path, data)
-	tmpl, err := parseMainLayout()
-	if err != nil {
-		InternalError(w, r)
-		return
-	}
-	executeTemplate(w, tmpl, mainLayout, TmplContext{Content: template.HTML(tmplString), Title:title})
+	executeTemplate(w, parsedMainLayout, mainLayout, TmplContext{Content: template.HTML(tmplString), Title:title})
 }
 
 func renderLayoutWithoutSidebar(w http.ResponseWriter, r *http.Request, t data.TemplateInfo, title string, data interface{}) {
 	tmplString, _ := execTmplToString(t.Name, t.Path, data)
-	tmpl, err := parseLayoutWithoutSidebar()
+	executeTemplate(w, parsedLayoutWithoutSidebar, layoutWithoutSidebar, TmplContext{Content: template.HTML(tmplString), Title:title})
+}
+
+type BlogMetadata struct {
+	ID               int64  `json:"id"`
+	Name             string `json:"name"`
+	Title            string `json:"title"`
+	TmplFileName     string `json:"tmplFileName"`
+	Author           string `json:"author"`
+	PublishDate      int64 `json:"publishDate"`
+	Tags             string `json:"tags"`
+	CommentCount     int64 `json:"commentCount"`
+	ImageUrl         string `json:"imageUrl"`
+	CommentsFileName string `json:"commentsFileName"`
+	Summary          string `json:"summary"`
+}
+
+func GetPostInfo(pName, fileName string, pos int64) (BlogMetadata, error) {
+	blogMeta := BlogMetadata{}
+	blogs, err := GetAllPostsOfPage(fileName)
 	if err != nil {
-		InternalError(w, r)
-		return
+		return blogMeta, err
 	}
-	executeTemplate(w, tmpl, layoutWithoutSidebar, TmplContext{Content: template.HTML(tmplString), Title:title})
+	blogMeta = blogs[pos]
+	return blogMeta, nil
+}
+
+func GetAllPostsOfPage(fileName string) ([]BlogMetadata, error) {
+	var blogsMetadata []BlogMetadata
+	_, err := os.Stat("data/blogs/" + fileName)
+	if err != nil {
+		return blogsMetadata, err
+	}
+	blogsData, err := ioutil.ReadFile("data/blogs/" + fileName)
+	if err != nil {
+		return blogsMetadata, err
+	}
+	if err := json.Unmarshal(blogsData, &blogsMetadata); err != nil {
+		return blogsMetadata, err
+	}
+	return blogsMetadata, nil
+}
+
+func GetFileName(page int64) (string, int64, int64) {
+	var fileName string
+	var t, max int64 = 1, data.BlogMetaInfo.TotalBlogs
+	if page > 0 && (page * pagination) <= data.BlogMetaInfo.TotalBlogs {
+		t = (page * pagination) / 20
+		fileName = "blog-" + strconv.FormatInt(t * 20 + 1, 10) + "-to-" + strconv.FormatInt(t * 20 + 20, 10) + ".json"
+		max = pagination
+	} else {
+		page = 1
+		t = 1
+		max = data.BlogMetaInfo.TotalBlogs
+		fileName = "blog-1-to-20.json"
+	}
+	return fileName, max, t
 }
